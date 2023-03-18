c1e6b3c90cc57ef3f20851debd428b8d6b0bde75
Aaron <hourglassprograms@gmail.com>
2023-03-18 13:05:06 +0000

    Added configured

--- EasyCATConfigurer/tbmcontrolpi_EasyCat.h
+++ EasyCATConfigurer/tbmcontrolpi_EasyCat.h
@@ -1,3 +1,5 @@
+#ifndef C1FC7438_468A_4154_930F_5E2D6EEC349E
+#define C1FC7438_468A_4154_930F_5E2D6EEC349E
 #ifndef CUSTOM_PDO_NAME_H
 #define CUSTOM_PDO_NAME_H
 
@@ -5,21 +7,29 @@
 //                                                                   //
 //     This file has been created by the Easy Configurator tool      //
 //                                                                   //
-//     Easy Configurator project TestEasyCAT_Custom.prj
+//     Easy Configurator project tbmcontrolpi.prj
 //                                                                   //
 //-------------------------------------------------------------------//
 
-#define CUST_BYTE_NUM_OUT 1
-#define CUST_BYTE_NUM_IN 8
-#define TOT_BYTE_NUM_ROUND_OUT 4
-#define TOT_BYTE_NUM_ROUND_IN 8
+#define CUST_BYTE_NUM_OUT 36
+#define CUST_BYTE_NUM_IN 16
+#define TOT_BYTE_NUM_ROUND_OUT 36
+#define TOT_BYTE_NUM_ROUND_IN 16
 
 typedef union //---- output buffer ----
 {
 	uint8_t Byte[TOT_BYTE_NUM_ROUND_OUT];
 	struct
 	{
-		uint8_t Leds;
+		int32_t temperatureTBM;
+		int32_t inclionmeter0;
+		int32_t inclionmeter1;
+		int32_t inclionmeter2;
+		int32_t methane;
+		int32_t input0;
+		int32_t input1;
+		int32_t input2;
+		int32_t input3;
 	} Cust;
 } PROCBUFFER_OUT;
 
@@ -28,12 +38,13 @@ typedef union //---- input buffer ----
 	uint8_t Byte[TOT_BYTE_NUM_ROUND_IN];
 	struct
 	{
-		uint16_t Analog_0;
-		uint16_t Analog_1;
-		uint16_t Bit16_RisingTestRamp;
-		uint8_t DipSwitches;
-		uint8_t Bit8_FallingTestRamp;
+		int32_t input0;
+		int32_t input1;
+		int32_t input2;
+		int32_t input3;
 	} Cust;
 } PROCBUFFER_IN;
 
 #endif
+
+#endif /* C1FC7438_468A_4154_930F_5E2D6EEC349E */
