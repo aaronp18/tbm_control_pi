1820596761f067b8e2b567753704f6492bf4a309
Matthew Knight <mrg.knight1@gmail.com>
2023-03-27 14:25:04 -0500

    add MQTT sender

---
+++ src/MQTT.cpp
@@ -0,0 +1,32 @@
+#include <mqtt/client.h>
+#include <chrono>
+
+const char * const BoolToString(bool b)
+{
+  return b ? "true" : "false";
+}
+
+class MQTT {
+    private:
+        std::string ip = "localhost:1883"; //TODO: change to real IP
+        std::string team = "Warwick Boring";
+        uint timestamp;
+        std::string msg;
+    public:
+        int send(bool mining, float latitude, float longitude, float heading_deg, float depth_m, float length_m) {
+            mqtt::client client(ip, "publisher", mqtt::create_options(MQTTVERSION_5));
+            client.connect();
+            timestamp = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+            msg = "{'team': '" + team + "', 'timestamp': " + std::to_string(timestamp) + ", 'mining': " + BoolToString(mining) + ", 'latitude': " + std::to_string(latitude) + ", 'longitude': " + std::to_string(longitude) + ", 'heading_deg': " + std::to_string(heading_deg) + ", 'depth_m': " + std::to_string(depth_m) + ", 'length_m': " + std::to_string(length_m) + "}";
+            mqtt::message_ptr pubmsg = mqtt::make_message("nabc23/warwick_boring", "");
+            pubmsg->set_payload(msg);
+            client.publish(pubmsg);
+            return (0); //success
+        }
+};
+
+int main() {
+    MQTT mqtt;
+    mqtt.send(true, 1.0, 2.0, 3.0, 4.0, 5.0);
+    return 0;
+}
